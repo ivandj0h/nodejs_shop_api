3a59c5ae44aca17940a2a4d91c4aedebe96fd0c7
IVANDI <41560792+Ivandidjoh@users.noreply.github.com>
2019-03-08 22:45:18 +0700

    adding functionality on app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.status(200).json({
+        message: 'It Works!'
+    });
+});
+
+module.exports = app;
