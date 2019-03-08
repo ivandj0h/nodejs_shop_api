f52d390ebce6acbb55d3d124235b79bb26d55cbe
IVANDI <41560792+Ivandidjoh@users.noreply.github.com>
2019-03-08 23:44:25 +0700

    Adding More Routes to the API (product.js)

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
+// This Route is use to GET all data
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'It Works! SomeOne access to /products using GET Request'
     });
 });
 
+// This Route is use to GET specific data based on id
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
     if(id === '1'){
@@ -22,9 +24,24 @@ router.get('/:productId', (req, res, next) => {
     }
 });
 
+// This Route is use to POST data to server
 router.post('/', (req, res, next) => {
     res.status(200).json({
         message: 'It Works! SomeOne access to /products using POST Request'
+    });
+});
+
+// This Route is use to UPDATE data to server
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Product Updated Successfully!!'
+    });
+});
+
+// This Route is use to DELETE/REMOVE data from server
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Product Deleted Successfully!!'
     });
 });
 
