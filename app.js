21d418a45407bbfd988d187f16e8841c231df4e7
IVANDI <41560792+Ivandidjoh@users.noreply.github.com>
2019-03-09 00:54:22 +0700

    added Error Handling on App.js

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
 const producRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+// Use Morgan as Midleware
+app.use(morgan('dev'));
 
 // this code is use to execute /products
 app.use('/products', producRoutes);
 
 // this code is use to execute /orders
 app.use('/orders', orderRoutes);
+
+// Handle Error for All Request
+app.use((req, res, next) => {
+    const error = new Error('Sorry!, Not Found!');
+
+    error.status(404);
+    next(error);
+});
+
+// Handling Other Error for All Request
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 
 
 module.exports = app;
