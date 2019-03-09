aa946e3b29f7ab027907ad4f7e76e0b1facd30f3
IVANDI <41560792+Ivandidjoh@users.noreply.github.com>
2019-03-09 16:40:36 +0700

    modified order route

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,41 +1,125 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const checkAuth = require('../middleware/check-auth');
+
+const Order = require("../models/order");
+const Product = require("../models/product");
 
 // This Route is use to GET all data
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'It Works! Your Order were Fetched!'
-    });
-});
+router.get("/", checkAuth, (req, res, next) => {
+    Order.find()
+      .select("product quantity _id")
+      .populate('product', 'name')
+      .exec()
+      .then(docs => {
+        res.status(200).json({
+          count: docs.length,
+          orders: docs.map(doc => {
+            return {
+              _id: doc._id,
+              product: doc.product,
+              quantity: doc.quantity,
+              request: {
+                type: "GET",
+                url: "http://localhost:3000/orders/" + doc._id
+              }
+            };
+          })
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err
+        });
+      });
+  });
 
 // This Route is use to GET specific data based on id
-router.get('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'It Works! This is Your Order Details!',
-        orderId: req.params.orderId
-    });
-});
+router.get("/:orderId", checkAuth, (req, res, next) => {
+    Order.findById(req.params.orderId)
+      .populate('product')
+      .exec()
+      .then(order => {
+        if (!order) {
+          return res.status(404).json({
+            message: "Order not found"
+          });
+        }
+        res.status(200).json({
+          order: order,
+          request: {
+            type: "GET",
+            url: "http://localhost:3000/orders"
+          }
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err
+        });
+      });
+  });
 
 // This Route is use to POST data to Server
-router.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    };
-    res.status(201).json({
-        status: '201',
-        message: 'It Works! Your Order were Created!',
-        order: order
-    });
-});
+router.post("/", checkAuth, (req, res, next) => {
+    Product.findById(req.body.productId)
+      .then(product => {
+        if (!product) {
+          return res.status(404).json({
+            message: "Product not found"
+          });
+        }
+        const order = new Order({
+          _id: mongoose.Types.ObjectId(),
+          quantity: req.body.quantity,
+          product: req.body.productId
+        });
+        return order.save();
+      })
+      .then(result => {
+        console.log(result);
+        res.status(201).json({
+          message: "Order stored",
+          createdOrder: {
+            _id: result._id,
+            product: result.product,
+            quantity: result.quantity
+          },
+          request: {
+            type: "GET",
+            url: "http://localhost:3000/orders/" + result._id
+          }
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
+  });
 
 // This Route is use to DELETE/REMOVE data from server
-router.delete('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'It Works! Your Order was Successfully Deleted!',
-        orderId: req.params.orderId
-    });
-});
+router.delete("/:orderId", checkAuth, (req, res, next) => {
+    Order.remove({ _id: req.params.orderId })
+      .exec()
+      .then(result => {
+        res.status(200).json({
+          message: "Order deleted",
+          request: {
+            type: "POST",
+            url: "http://localhost:3000/orders",
+            body: { productId: "ID", quantity: "Number" }
+          }
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err
+        });
+      });
+  });
 
 
 module.exports = router;
